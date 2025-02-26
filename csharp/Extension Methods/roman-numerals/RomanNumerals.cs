0333657ed67ae190f1db729e0a810fb890d73334
Kevin Brennan <krb.brennan@gmail.com>
2025-02-25 21:48:05 -0500

    PROG: Extension methods -- "Roman Numeals"

---
+++ csharp/Extension Methods/roman-numerals/RomanNumerals.cs
@@ -0,0 +1,7 @@
+public static class RomanNumeralExtension
+{
+    public static string ToRoman(this int value)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
