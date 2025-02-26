6a96b86d69f7be832c7a94227e4ce6aefe7f2e51
Kevin Brennan <krb.brennan@gmail.com>
2025-02-25 20:01:36 -0500

    DONE: Floats exercise 1 -- "Interest is Interesting"

--- csharp/Do While/interest-is-interesting/InterestIsInteresting.cs
+++ csharp/Do While/interest-is-interesting/InterestIsInteresting.cs
@@ -10,28 +10,39 @@ static class SavingsAccount
         else if (balance < 1000)
         {
             return (float) 0.5;
-        } else if (balance >= 1000)
+        } else if ((balance >= 1000) && balance < 5000)
         {
             return (float) 1.621;
         } else
         {
-            return (float)2.475;
+            return (float) 2.475;
         }
     }
 
     public static decimal Interest(decimal balance)
     {
         //throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
-
+        return ((decimal) InterestRate(balance) / 100) * (decimal) balance;
     }
 
     public static decimal AnnualBalanceUpdate(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
+        //throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
+        return Interest(balance) + balance;
     }
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        //throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        int years = 0;
+        decimal updatedYearlyBalance = balance;
+        // DO WHILE TIME BABY
+        do
+        {
+            updatedYearlyBalance = AnnualBalanceUpdate(updatedYearlyBalance);
+            years += 1;
+        }
+        while (updatedYearlyBalance < targetBalance);
+        return years;
     }
 }
