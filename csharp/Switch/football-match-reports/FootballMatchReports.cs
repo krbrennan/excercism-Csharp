0ad5d74b0bad8c87af559f5ac7366ba38ce97688
Kevin Brennan <krb.brennan@gmail.com>
2025-03-03 19:54:27 -0500

    DONE: Switch -- football match report.
    I've learned that I can "go to definition" of classes in the test cases to fill in the massive gaps in explanation exercism leaves with these exercises.

--- csharp/Switch/football-match-reports/FootballMatchReports.cs
+++ csharp/Switch/football-match-reports/FootballMatchReports.cs
@@ -1,12 +1,73 @@
+
 public static class PlayAnalyzer
 {
+
+    //this wasn't needed because the final default condition of a swich handles it
+    //public static bool IsNumberValue(int num)
+    //{
+    //    return num > 11;
+    //}
     public static string AnalyzeOnField(int shirtNum)
     {
-        throw new NotImplementedException($"Please implement the (static) PlayAnalyzer.AnalyzeOnField() method");
+        //if(!IsNumberValue(shirtNum))
+        //{
+        //    return "";
+        //}
+
+        switch (shirtNum)
+        {
+            case 1:
+                return "goalie";
+                break;
+            case 2:
+                return "left back";
+            case int i when i == 3 || i == 4:
+                return "center back";
+            case 5:
+                return "right back";
+            case int i when i == 6 || i == 7 || i == 8:
+                return "midfielder";
+            case 9:
+                return "left wing";
+            case 10:
+                return "striker";
+            case 11:
+                return "right wing";
+            default:
+                return "UNKNOWN";
+        }
     }
 
     public static string AnalyzeOffField(object report)
     {
-        throw new NotImplementedException($"Please implement the (static) PlayAnalyzer.AnalyzeOffField() method");
+
+        switch (report)
+        {
+            case int i:
+                return $"There are {i} supporters at the match.";
+                break;
+            case string s:
+                return s;
+                break;
+            case Foul f:
+                return f.GetDescription();
+                break;
+            case Injury j:
+                return $"Oh no! {j.GetDescription()} Medics are on the field.";
+            case Incident inc:
+                return inc.GetDescription();
+            case Manager mg:
+                // check if manager has a team and return appropriately
+                if(mg.Club == null)
+                {
+                    return mg.Name;
+                } else
+                {
+                    return $"{mg.Name} ({mg.Club})";
+                }
+                    default:
+                return "";
+                break;
+        }
     }
 }
