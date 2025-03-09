3647cab19da9ec17dfad1a9e6e0a4b8962c7cd66
Kevin Brennan <krb.brennan@gmail.com>
2025-03-08 21:33:53 -0500

    DONE: Characters -- squeaky clean.
    I kept my original implementation commented out. It was getting really difficult debugging the "combine_conversions" test case and so I scrapped it to make it a little DRY'er and plus I needed to Frankenstien's monster the logic of it. Kind of ugly implementation but I did leave comments.

--- csharp/Characters/squeaky-clean/SqueakyClean.cs
+++ csharp/Characters/squeaky-clean/SqueakyClean.cs
@@ -1,105 +1,165 @@
-
-
 using System.Net.NetworkInformation;
 using System.Text;
 
 public static class Identifier
 
 {
-
-    public static char[] ConvertToChars(string word)
-    {
-        return word.ToCharArray();
-    }
-
-    public static string CamelCase(string word)
-    {
-        char[] chars = ConvertToChars(word);
-        int indexToChange = word.IndexOf("-") + 1;
-        chars[indexToChange] = char.ToUpper(chars[indexToChange]);
-        string camelCaseWord = new string(chars);
-        camelCaseWord = camelCaseWord.Replace("-", "");
-        return camelCaseWord;
-    }
-
-    public static string removeCtrl(string word)
-    {
-        string tempWord = "";
-        char[] chars = ConvertToChars(word);
-
-        foreach(char c in chars)
-        {
-            if (!char.IsControl(c))
-            {
-                tempWord += c;
-            }
-            else
-            {
-                tempWord += "CTRL";
-            }
-        }
-            return tempWord;
-    }
-
-    public static string RemoveNums(string word)
-    {
-        char[] chars = ConvertToChars(word);
-        string final = "";
-
-        foreach (char c in chars)
-        {
-            if (c.ToString() == "_")
-            {
-                final += c;
-            } else if (!char.IsDigit(c))
-            {
-                final += c;
-            }
-        }
-        return final;
-    }
-
-
-
-    public static bool IsGreek(char c)
-    {
-        return c >= '\u03B1' && c <= '\u03C9';
-    }
-
-    public static string RemoveGreek(string word)
-    {
-        string final = "";
-        foreach (var c in word.ToCharArray())
-        {
-            if (!IsGreek(c))
-            {
-                final += c;
-            }
-        }
-        return final;
-    }
-
-
     public static string Clean(string identifier)
     {
-        string cleanedString = "";
-        cleanedString = identifier.Replace(" ", "_");
+        string finalWord = "";
+        char[] chardWord = identifier.ToCharArray();
 
-        // make camelCase
-        if (identifier.Contains("-"))
+        for (int i = 0; i < chardWord.Length; i++)
         {
-            cleanedString = CamelCase(identifier);
+            char c = chardWord[i];
+            // gets rid of emoji jawns,  lowercase greek letters, numbers, control characters
+            if (char.IsSurrogate(c) ||
+                c >= '\u03B1' && c <= '\u03C9' ||
+                char.IsDigit(c)
+                )
+            {
+                continue;
+            }
+            else if (char.IsControl(c))
+            {
+                // replace control characters with CTRL
+                finalWord += "CTRL";
+                // continue is needed because otherwise the control character will for some reason still be added?
+                continue;
+            }
+            switch (c)
+            {
+                case ' ':
+                    finalWord += "_";
+                    break;
+                case '-':
+                    finalWord += char.ToUpper(chardWord[i + 1]);
+                    i++;
+                    break;
+                default:
+                    finalWord += c;
+                    break;
+            }
         }
-        // remove ctrl characters
-        cleanedString = removeCtrl(cleanedString);
-        cleanedString = RemoveNums(cleanedString);
-        cleanedString = RemoveGreek(cleanedString);
-
-        //cleanedString = LettersOnly(cleanedString);
-
-        //cleanedString = RemoveNonLetters(cleanedString);
-
-
-        return cleanedString;
+        return finalWord;
     }
+
+
+    //public static char[] ConvertToChars(string word)
+    //{
+    //    return word.ToCharArray();
+    //}
+
+    //public static string CamelCase(string word)
+    //{
+    //    char[] chars = ConvertToChars(word);
+    //    int indexToChange = word.IndexOf("-") + 1;
+    //    chars[indexToChange] = char.ToUpper(chars[indexToChange]);
+    //    string camelCaseWord = new string(chars);
+    //    camelCaseWord = camelCaseWord.Replace("-", "");
+    //    return camelCaseWord;
+    //}
+
+    //public static string removeCtrl(string word)
+    //{
+    //    string tempWord = "";
+    //    char[] chars = ConvertToChars(word);
+
+    //    foreach(char c in chars)
+    //    {
+    //        if (!char.IsControl(c))
+    //        {
+    //            tempWord += c;
+    //        }
+    //        else
+    //        {
+    //            tempWord += "CTRL";
+    //        }
+    //    }
+    //        return tempWord;
+    //}
+
+    //public static string RemoveNums(string word)
+    //{
+    //    char[] chars = ConvertToChars(word);
+    //    string final = "";
+
+    //    foreach (char c in chars)
+    //    {
+    //        if (c == '-')
+    //        {
+    //            final += "_";
+    //            //final += c;
+    //        } else if (!char.IsDigit(c))
+    //        {
+    //            final += c;
+    //        }
+    //    }
+    //    return final;
+    //}
+
+
+
+    //public static bool IsGreek(char c)
+    //{
+    //    return c >= '\u03B1' && c <= '\u03C9';
+    //}
+
+    //public static string RemoveGreek(string word)
+    //{
+    //    string final = "";
+    //    foreach (var c in word.ToCharArray())
+    //    {
+    //        if (!IsGreek(c))
+    //        {
+    //            final += c;
+    //        }
+    //    }
+    //    return final;
+    //}
+
+    //public static string removeSurrogateChars(string cleanedString)
+    //{
+    //    char[] chars = cleanedString.ToCharArray();
+    //    string final = "";
+    //    foreach(char c in chars)
+    //    {
+    //        if (!char.IsSurrogate(c))
+    //        {
+    //            final += c;
+    //        } else if(c == '-')
+    //        {
+    //            final += "_";
+    //        }
+    //    }
+    //    return final;
+    //}
+
+
+    //public static string Clean(string identifier)
+    //{
+    //    string cleanedString = "";
+    //    cleanedString = identifier.Replace(" ", "_");
+
+    //    // make camelCase
+    //    if (identifier.Contains("-"))
+    //    {
+    //        cleanedString = CamelCase(identifier);
+    //    }
+    //    // remove ctrl characters
+    //    cleanedString = removeCtrl(cleanedString);
+    //    cleanedString = RemoveNums(cleanedString);
+    //    cleanedString = RemoveGreek(cleanedString);
+    //    cleanedString = removeSurrogateChars(cleanedString);
+
+
+    //    //cleanedString = LettersOnly(cleanedString);
+
+    //    //cleanedString = RemoveNonLetters(cleanedString);
+
+
+    //    return cleanedString;
+    //}
+
 }
+
