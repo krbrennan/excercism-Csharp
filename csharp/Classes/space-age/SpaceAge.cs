050ee40db6c57de32e7fb5663b6315ddc33f09dd
Kevin Brennan <krb.brennan@gmail.com>
2025-03-08 20:43:08 -0500

    DONE: Classes -- space age

--- csharp/Classes/space-age/SpaceAge.cs
+++ csharp/Classes/space-age/SpaceAge.cs
@@ -1,46 +1,71 @@
 public class SpaceAge
 {
+    Dictionary<string, double> timeDict = new Dictionary<string, double>
+    {
+        {"Mercury", 0.2408467 },
+        {"Venus", 0.61519726 },
+        {"Earth", 1.0 },
+        {"Mars", 1.8808158 },
+        {"Jupiter", 11.862615 },
+        {"Saturn", 29.447498 },
+        {"Uranus", 84.016846 },
+        {"Neptune", 164.79132 }
+    };
+    
+    private static int _seconds;
     public SpaceAge(int seconds)
     {
+        // create date object
+        _seconds = seconds;
+    }
+
+    public double GetTimeSpan(double relativeRatio)
+    {
+        //double 
+        TimeSpan ts = TimeSpan.FromSeconds(_seconds);
+        double age = ts.TotalDays / 365.25;
+        double relativeAge = age * 1/relativeRatio;
+
+        return Math.Round(relativeAge, 2);
     }
 
     public double OnEarth()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return GetTimeSpan(timeDict["Earth"]);
     }
 
     public double OnMercury()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return GetTimeSpan(timeDict["Mercury"]);
     }
 
     public double OnVenus()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return GetTimeSpan(timeDict["Venus"]);
     }
 
     public double OnMars()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return GetTimeSpan(timeDict["Mars"]);
     }
 
     public double OnJupiter()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return GetTimeSpan(timeDict["Jupiter"]);
     }
 
     public double OnSaturn()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return GetTimeSpan(timeDict["Saturn"]);
     }
 
     public double OnUranus()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return GetTimeSpan(timeDict["Uranus"]);
     }
 
     public double OnNeptune()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return GetTimeSpan(timeDict["Neptune"]);
     }
 }
