c8d7847ba1d7726159ecf6316838bc840c155542
Kevin Brennan <krb.brennan@gmail.com>
2025-03-10 16:26:01 -0400

    DONE: DateTime -- gigaseconds
    Used TimeSpan to add 1 billion seconds to the provided DateTime moment.

--- csharp/Datetimes/gigasecond/Gigasecond.cs
+++ csharp/Datetimes/gigasecond/Gigasecond.cs
@@ -1,7 +1,13 @@
 public static class Gigasecond
 {
+
+    // a gigasecond in one-thousand-million seconds, or a billion for those of us who speak with precision and who rely on convention rather than abstraction in order to communicate with others. People who say "one-thousand-million" are trite.
     public static DateTime Add(DateTime moment)
     {
+        int giga = 1_000_000_000;
 
+        TimeSpan gigaTimespan = TimeSpan.FromSeconds(giga);
+
+        return moment.Add(gigaTimespan);
     }
 }
